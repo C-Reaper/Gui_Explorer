4f9bd5fc4a1ef0a6f5fb72fb084e3ab6a02db030
codeleaded <gnu.c.reaper@gmail.com>
2026-02-08 19:42:13 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -6,7 +6,7 @@
 #include "/home/codeleaded/System/Static/Library/Explorer.h"
 #elif defined _WIN32
 #include "F:/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
-#include "/home/codeleaded/System/Static/Library/Explorer.h"
+#include "F:/home/codeleaded/System/Static/Library/Explorer.h"
 #elif defined(__APPLE__)
 #error "Apple not supported!"
 #else
@@ -19,10 +19,22 @@ Explorer ex;
 void Setup(AlxWindow* w){
 	ex = Explorer_New("..",Rect_New(
 		(Vec2){ 10.0f,10.0f },
-		(Vec2){ 500.0f,GetHeight() - 20 }
+		(Vec2){ 200.0f,GetHeight() - 20 }
 	));
 }
 void Update(AlxWindow* w){
+	if(Stroke(ALX_KEY_W).DOWN) ex.r.d.x -= 100.0f * w->ElapsedTime;
+	if(Stroke(ALX_KEY_S).DOWN) ex.r.d.x += 100.0f * w->ElapsedTime;
+	if(Stroke(ALX_KEY_A).DOWN) ex.r.p.x -= 100.0f * w->ElapsedTime;
+	if(Stroke(ALX_KEY_D).DOWN) ex.r.p.x += 100.0f * w->ElapsedTime;
+
+	if(Stroke(ALX_MOUSE_L).PRESSED){
+		Explorer_File* ef = Explorer_Select(&ex,w->Strokes,GetMouse());
+		if(ef){
+			printf("%s\n",ef->path);
+		}
+	}
+	
 	Explorer_Update(&ex,w->Strokes,GetMouse());
 
 	Clear(BLACK);
